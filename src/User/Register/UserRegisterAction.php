b0716effcf8a9d7fdc373eeedc20632552ee195f
ahundiak <ahundiak@gmail.com>
2018-07-13 14:54:49 -0500

    Cerad Core

---
+++ src/User/Register/UserRegisterAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahundiak
+ * Date: 7/13/18
+ * Time: 2:26 PM
+ */
+
+namespace App\User\Register;
+
+
+class UserRegisterAction
+{
+
+}
---
+++ src/User/Register/UserRegisterForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahundiak
+ * Date: 7/13/18
+ * Time: 2:25 PM
+ */
+
+namespace App\User\Register;
+
+
+class UserRegisterForm
+{
+
+}
